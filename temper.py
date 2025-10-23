f0a5279b2ff7ad110dbab3837cf96caf7f41b461
veeresh <veeresh@123>
2025-10-23 16:55:02 +0530

    save to branch 1

--- temper.py
+++ temper.py
@@ -9,3 +9,18 @@ else:
 
 print(f"Temperature: {temp}°C")
 print("Status:", status)
+
+temp = float(input("Enter temperature in °C: "))
+
+if temp < 20:
+    status = "Cold"
+elif temp <= 30:
+    status = "Normal"
+else:
+    status = "Hot"
+
+fahrenheit = (temp * 9/5) + 32
+
+print(f"Temperature: {temp}°C")
+print("Status:", status)
+print(f"Temperature in Fahrenheit: {fahrenheit}°F")
